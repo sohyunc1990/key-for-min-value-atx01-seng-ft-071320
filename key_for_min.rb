65d0cd9f49a215cc4f2f256b637193a6b54ae3d2
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-07 23:32:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,14 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require 'pry'
 def key_for_min_value(name_hash)
+  smallest=9999
+  asdf=[]
+  name_hash.each do |name, value|
+    if smallest < name
+     return delete.name = asdf
+  end
+puts asdf
+end
 
 end
